fdbfb0209c050eebf078b482f98ebc120e610d8e
Vickycp <50542501+Vickycp@users.noreply.github.com>
2025-06-15 13:39:40 +0530

    Update resque.rb

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,9 +1,13 @@
-Resque.redis = Redis.new(
-  host:     ENV["REDIS_HOST"],
-  port:     ENV["REDIS_PORT"],
-  password: ENV["REDIS_PASSWORD"],
-  thread_safe: true
-)
+if ENV["REDIS_URL"].present?
+  Resque.redis = Redis.new(url: ENV["REDIS_URL"], thread_safe: true)
+else
+  Resque.redis = Redis.new(
+    host:     ENV["REDIS_HOST"],
+    port:     ENV["REDIS_PORT"],
+    password: ENV["REDIS_PASSWORD"],
+    thread_safe: true
+  )
+end
 
 if ENV["RESQUE_NAMESPACE"].present?
   Resque.redis.namespace = ENV["RESQUE_NAMESPACE"].to_sym
